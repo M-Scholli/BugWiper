32b73024fbc8f4118dfb5dbd754135241773e514
Moritz Scholjegerdes <moritz-scholjegerdes@gmx.com>
2015-06-19 15:38:54 +0200

    Add L298 files for the motor driver

---
+++ L298.cpp
@@ -0,0 +1,9 @@
+/*
+ * L298.cpp
+ *
+ *  Created on: 19.06.2015
+ *      Author: Moritz Scholjegerdes
+ */
+
+#includ	"L298.h"
+
---
+++ L298.h
@@ -0,0 +1,14 @@
+/*
+ * L298.h
+ *
+ *  Created on: 19.06.2015
+ *      Author: Moritz Scholjegerdes
+ */
+
+#ifndef L298_H_
+#define L298_H_
+
+
+
+
+#endif /* L298_H_ */
