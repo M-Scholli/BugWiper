edab39affd7b4b5defed8543344217a1bd4d87fa
Moritz Scholjegerdes <moritz-scholjegerdes@gmx.com>
2015-06-20 19:02:49 +0200

    Button: add Button.cpp und .hpp to check for long or short press of buttons

---
+++ Button.cpp
@@ -0,0 +1,57 @@
+/*
+ * Button.cpp
+ *
+ *  Created on: 20.06.2015
+ *      Author: Moritz Scholjegerdes
+ */
+
+#include "Button.h"
+#include "Arduino.h"
+
+Button::Button(byte pin, unsigned int debounce_delay, unsigned int long_delay)
+{
+	pinMode(pin, INPUT);
+	digitalWrite(pin, HIGH);
+	_pin=pin;
+	_debounce_delay=debounce_delay;
+	_long_delay=long_delay;
+	_button_press = 0;
+}
+
+void Button::check_button_state()
+{
+	boolean button_state = digitalRead(_pin);
+
+	if (button_state == LOW && _button_press == 0){
+		time_of_last_press=millis();
+		_button_press= 1;
+	}
+
+	// In case that millis() was reseted, write time_of_last_press new.
+	if (millis() < time_of_last_press){
+		time_of_last_press=millis();
+	}
+
+	time_since_last_press = millis() - time_of_last_press;
+	if (button_state == HIGH && _button_press == 1 && time_since_last_press >= _debounce_delay ){
+		if(time_since_last_press >= _long_delay) _long_pressed=1;
+		else _short_pressed=1;
+		_button_press=0;
+	}
+}
+
+byte Button::button_pressed_long()
+{
+	byte pressed;
+	pressed=_long_pressed;
+	_long_pressed = 0;
+	return pressed;
+}
+
+byte Button::button_pressed_short()
+{
+	byte pressed;
+	pressed=_short_pressed;
+	_short_pressed = 0;
+	return pressed;
+}
---
+++ Button.h
@@ -0,0 +1,35 @@
+/*
+ * Button.h
+ *
+ *  Created on: 20.06.2015
+ *      Author: Moritz Scholjegerdes
+ *
+ *      Checking if a button was pressed long or short with debounce for active low Buttons with use of the internal pullups.
+ *
+ */
+
+#ifndef BUTTON_H_
+#define BUTTON_H_
+
+#include "Arduino.h"
+
+class Button
+{
+	public:
+		Button(byte pin, unsigned int debounce_delay, unsigned int long_delay);
+		void check_button_state();
+		byte button_pressed_long();
+		byte button_pressed_short();
+	private:
+		byte _pin;
+		byte _button_press; // button is actual pressed
+		byte _long_pressed; 	// button was pressed long
+		byte _short_pressed;	// button was pressed short
+		unsigned int _debounce_delay;
+		unsigned int _long_delay;
+		unsigned long time_of_last_press;
+		unsigned long time_since_last_press;
+};
+
+
+#endif /* BUTTON_H_ */
