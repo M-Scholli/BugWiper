aaf803d35bc7fce680730021cc0a83a7ba0e1099
Moritz Scholjegerdes <moritz-scholjegerdes@gmx.com>
2015-06-22 22:40:30 +0200

    Wiper: add class Wiper

--- src/Wiper.h
+++ src/Wiper.h
@@ -8,6 +8,20 @@
 #ifndef WIPER_H_
 #define WIPER_H_
 
+class Wiper
+{
+public:
+  Wiper (L298 motor, int max_time_s, int min_time_s );
+  byte
+  wipe();
+  void
+  stop();
+private:
+  void
+  read_direction ();
+  void
+  update_direction();
+};
 
 
 #endif /* WIPER_H_ */
