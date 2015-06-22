c6a5b92ed87a5c963a38d7ab4f8dd96a5197bcdb
Moritz Scholjegerdes <moritz-scholjegerdes@gmx.com>
2015-06-22 23:59:11 +0200

    Wiping: add class Wiping and function w_wiping

--- src/Wiping.cpp
+++ src/Wiping.cpp
@@ -7,6 +7,21 @@
 
 #include "Wiping.h"
 #include "Arduino.h"
+#include "L298.h"
+
+Wiping::Wiping (L298, byte p_start, byte t_min, byte t_max)
+{
+  _p_start = p_start;
+  _t_min = t_min;
+  _t_max = t_max;
+}
+
+byte
+Wiping::w_wiping (byte direction)
+{
+  byte safe = 0;
+  return safe;
+}
 
 void
 fullWipe ()
--- src/Wiping.h
+++ src/Wiping.h
@@ -8,6 +8,21 @@
 #ifndef WIPE_H_
 #define WIPE_H_
 
+#include "Arduino.h"
+#include "L298.h"
+
+class Wiping
+{
+public:
+  Wiping (L298, byte p_start, byte t_min, byte t_max);
+  byte
+  w_wiping (byte direction);
+private:
+  byte _p_start;
+  byte _t_min;
+  byte _t_max;
+};
+
 void
 fullWipe ();
 
