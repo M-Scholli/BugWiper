e4145ab0ed5a36bd367d5ab6d3dd195997fefb19
Moritz Scholjegerdes <moritz-scholjegerdes@gmx.com>
2015-06-22 04:26:43 +0200

    Wiper: add new files Wiper.h and .cpp

---
+++ src/Wiper.cpp
@@ -0,0 +1,8 @@
+/*
+ * Wiper.cpp
+ *
+ *  Created on: 22.06.2015
+ *      Author: flying-m2
+ */
+
+
---
+++ src/Wiper.h
@@ -0,0 +1,13 @@
+/*
+ * Wiper.h
+ *
+ *  Created on: 22.06.2015
+ *      Author: Moritz Scholjegerdes
+ */
+
+#ifndef WIPER_H_
+#define WIPER_H_
+
+
+
+#endif /* WIPER_H_ */
