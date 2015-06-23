3ba8babe5872ef3f447929b64f99016fcbc7e5ca
Moritz Scholjegerdes <moritz-scholjegerdes@gmx.com>
2015-06-23 17:28:36 +0200

    LED: add LED.cpp and LED.h

---
+++ src/LED.cpp
@@ -0,0 +1,8 @@
+/*
+ * LED.cpp
+ *
+ *  Created on: 23.06.2015
+ *      Author: Moritz Scholjegerdes
+ */
+
+#include "LED.h"
---
+++ src/LED.h
@@ -0,0 +1,13 @@
+/*
+ * LED.h
+ *
+ *  Created on: 23.06.2015
+ *      Author: Moritz Scholjegerdes
+ */
+
+#ifndef LED_H_
+#define LED_H_
+
+
+
+#endif /* LED_H_ */
