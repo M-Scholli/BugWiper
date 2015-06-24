ad0ea3c281b0b2b1e4357b64af6e5109e78700a4
Moritz Scholjegerdes <moritz-scholjegerdes@gmx.com>
2015-06-24 03:56:30 +0200

    Wiper: uses now w_wiping and makes led1 on when something went wrong

--- src/Wiper.cpp
+++ src/Wiper.cpp
@@ -6,9 +6,11 @@
  */
 
 #include "Wiper.h"
+
 #include "Wiping.h"
 #include "Arduino.h"
 #include "L298.h"
+#include "../BugWiper.h"
 
 Wiper::Wiper ()
 {
@@ -18,11 +20,30 @@ Wiper::Wiper ()
 void
 Wiper::fullWipe ()
 {
+  byte safe;
+  byte direction;
+  // to do: read direction
+  safe = full_wiping.w_wiping (direction);
+  if (safe == 0)
+    {
+      led1.on ();
+    }
+  else
+    {
+      // to do: toggle direction
+    }
 
 }
 
 void
 Wiper::TieUp ()
 {
-
+  byte safe;
+  byte direction;
+  // to do: read direction
+  safe = tieing_up.w_wiping (direction);
+  if (safe == 0)
+    {
+      led1.on ();
+    }
 }
